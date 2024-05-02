502efd1fe94b0789fde36f4de0e047bf1994daf5
IsmaelSFerreira <ismaelsouzaferreira92@gmail.com>
2024-05-02 17:45:42 -0300

    Desafio Concluido

--- conta-banco/src/ContaTermina.java
+++ conta-banco/src/ContaTermina.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTermina {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!!!");
+
+        Scanner Scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o seu nome: ");
+        String nome = Scanner.nextLine();
+
+        System.out.println("Digite o número da sua conta bancária: ");
+        int numero = Scanner.nextInt();
+
+        System.out.println("Digite o número da agência: ");
+        String agencia = Scanner.next();
+
+        System.out.println("Digite o saldo da sua conta bancária: ");
+        double saldo = Scanner.nextDouble();
+
+        System.out.println("Olá "+ nome +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta "+ numero +" e seu saldo "+ saldo +" já está disponível para saque");
+
+
     }
 }
